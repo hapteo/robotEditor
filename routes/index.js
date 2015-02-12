5804b3a48a194fd87d0b07a6b4e0c12476e40d6c
Philippe Savary <savary.phi@gmail.com>
2015-02-12 16:53:55 +0100

    Add sample result of corenlp parsing results

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,19 @@ var router = express.Router();
 console.log("******* BE Patient : Core nlp is loading, it might take up to one minute… ");
 var NLP = require('stanford-corenlp');
 var config = {"nlpPath":"./lib/stanford-corenlp-3.4","version":"3.4"};
-//var coreNLP = new NLP.StanfordNLP(config)
-//coreNLP.loadPipelineSync();
+
+var isCorenlpEnabled = false;
+var coreNLP;
+var corenlpSample;
+
+//Set isCorenlpEnabled to true to enable corenlp parsing otherwise will use one of the sample result (corenlpSample.sample1, corenlpSample.sample2…) set in corenlpSamples.js
+if (isCorenlpEnabled) {
+	coreNLP = new NLP.StanfordNLP(config)
+	coreNLP.loadPipelineSync();
+}
+else 
+	corenlpSample = require('./corenlpSamples');
+
 console.log("******* Core nlp is loaded… ");
 
 
@@ -44,15 +55,21 @@ router.post('/analyze', function(req, res, next) {
 	if (params.string === undefined || params.string.length == 0) {
 		res.status(500).send("Body malformed : String parameter is missing");
 	} else {
-		//send text to coreNLP and return result with status 200
-	/*	coreNLP.process(params.string, function(err, result)*/ {
-			var needles = phraseCleaner.clean("result");
+		if (isCorenlpEnabled) {
+			//send text to coreNLP and return result with status 200		
+			coreNLP.process(params.string, function(err, result) {
+				//console.log('\n'+JSON.stringify(result));
+				var needles = phraseCleaner.clean(result);
+				var images = imageSearcher.searchImage(needles);
+				res.status(200).send(images);
+			});
+		} else {
+			//use corenlpSamples.samplexxx 		
+			//console.log('\n'+JSON.stringify(corenlpSample.sample1));
+			var needles = phraseCleaner.clean(corenlpSample.sample1);
 			var images = imageSearcher.searchImage(needles);
-			
-			//console.log(err,JSON.stringify(result));
 			res.status(200).send(images);
 		}
-		/*);*/
 	}
 
 });
