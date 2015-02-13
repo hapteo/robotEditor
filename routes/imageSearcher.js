27370584b4218ce6a5c06ffd98335988bfc9a5fd
Philippe Savary <savary.phi@gmail.com>
2015-02-13 10:30:13 +0100

    add returned response documentation

--- routes/imageSearcher.js
+++ routes/imageSearcher.js
@@ -6,6 +6,17 @@ var imageSearcher = exports = module.exports = {};
  *
  * @param {[Strings]} needles : List of cleaned strings to input into google image search engine
  * @return {[hash of images linked with keywords to output]]}}  
+ *{
+ * "investiture John Kennedy”: [
+ * 					{"http://img1_1.jpg” : {pertinence:100}},
+ * 					{"http://img1_2.jpg” : {pertinence:85}},
+ * 					{"http://img1_3.jpg” : {pertinence:75}}
+ * 					],
+ * "baie cochons 1961”: [
+ * 					{"http://img2_1.jpg” : {pertinence:100}},
+ * 					{"http://img2_2.jpg” : {pertinence:35}},
+ * 					]
+ * }
  */
 
 imageSearcher.searchImage = function(needles){
