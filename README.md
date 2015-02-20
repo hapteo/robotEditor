f07cc942db89e92b3a5f27aaf16af74a56abe6c3
Nakurai <nakurai@Avalon.on.snoopy>
2015-02-20 23:26:20 +0100

    Changed server arcitecture, finished webSearches

--- README.md
+++ README.md
@@ -8,17 +8,29 @@ RobotEditor est un POC.
 robotEditorServer est développé en javascript + node.js.
 
 
-## Dependencies
+## Dépendances
 Hapteo utilise les frameworks suivants :
 
 * [Standord CoreNLP](http://nlp.stanford.edu/software/corenlp.shtml)
 * Stanford-core-nlp dans sa version wrapper node.js [core-nlp-master](https://github.com/hiteshjoshi/node-stanford-corenlp).
+* Le module NodeJS [googleapis](https://www.npmjs.com/package/googleapis), et plus précisément la partie custom search
 
 La partie nlp nécessite l'installation de JAVA et JAVA sdk. robotEditorServer utilise [Stanford CoreNLP](http://nlp.stanford.edu/software/corenlp.shtml) v3.4 ou supérieur. Vous devrez installer ce framework dans le réperoire /lib et définir le chenin vers ce répertoire et sa version dans index.js (ex : var config = {"nlpPath":"./lib/stanford-corenlp-3.4","version":"3.4"};
 )
 Stanford Corenlp nécessite l'installation de JAVA. Ne pas oublier de définir comme variable d'environnement [JAVA_HOME](https://github.com/nearinfinity/node-java).
 
+L'utilisation de googleapis nécessite d'avoir à disposition:
+* une clef pour l'API custom search. Cette clef peut-être obtenue via [la console d'un projet](https://console.developers.google.com/project?_ga=1.123096587.1006488701.1424181039)
+* L'identifiant d'un moteur de recherhe personnalisé (CX). Un tel moteur peut être créé sur [cette page](https://www.google.com/cse/all). Il vaut mieux le paramétrer comme pouvant accéder à "tout internet"
 
+Ces deux clefs doivent ensuite être placées dans un fichier private_keys.js, qui n'est pas inclu dans les sources par mesure de sécurité. Il suffit de placer à l'intérieur le contenu suivant:
+
+```javascript
+var api = 'YOUR_API_KEY';
+var cx = 'CX_NUMBER';
+exports.api = api;
+exports.cx = cx;
+```
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,8 @@
 var express = require('express');
 var router = express.Router();
-
-console.log("******* BE Patient : Core nlp is loading, it might take up to one minute… ");
-var NLP = require('stanford-corenlp');
-var config = {"nlpPath":"./lib/stanford-corenlp-34","version":"3.4"};
-
-var isCorenlpEnabled = true;
-var coreNLP;
-var corenlpSample;
-
-//Set isCorenlpEnabled to true to enable corenlp parsing otherwise will use one of the sample result (corenlpSample.sample1, corenlpSample.sample2…) set in corenlpSamples.js
-if (isCorenlpEnabled) {
-	coreNLP = new NLP.StanfordNLP(config)
-	coreNLP.loadPipelineSync();
-}
-else 
-	corenlpSample = require('./corenlpSamples');
-
-console.log("******* Core nlp is loaded… ");
-
-
-var phraseCleaner = require ("./phraseCleaner")
-var imageSearcher = require ("./imageSearcher")
+var webSearcher = require ("./webSearcher");
+var nlp = require('./nlp.js').getInstance();
+var q = require('q');
 
 //get params from body
 var getParams  = function(req) { 
@@ -52,7 +33,9 @@ router.get('/test', function(req, res, next) {
 });
 
 
-/* POST analyze page with string. */
+/* POST analyze page with string. 
+TODO: sanitize user input !
+*/
 router.post('/analyze', function(req, res, next) {
 
 	var params = getParams(req);
@@ -62,24 +45,15 @@ router.post('/analyze', function(req, res, next) {
 		output="Body malformed : String parameter is missing";
 		res.render('test', { title: 'test', answer:output });
 	} else {
-		if (isCorenlpEnabled) {
-			//send text to coreNLP and return result with status 200		
-			coreNLP.process(params.string, function(err, result) {
-				output = JSON.stringify(result);
-				res.render('test', { title: 'test', answer:output });
-				//var needles = phraseCleaner.clean(result);
-				//var images = imageSearcher.searchImage(needles);
-				//res.status(200).send(images);
-			});
-		} else {
-			output = "user input is recognized !";
-			res.render('test', { title: 'test', answer:output });
-			//use corenlpSamples.samplexxx 		
-			//console.log('\n'+JSON.stringify(corenlpSample.sample1));
-			//var needles = phraseCleaner.clean(corenlpSample.sample1);
-			//var images = imageSearcher.searchImage(needles);
-			//res.status(200).send(images);
-		}
+		nlp.userTextAnalysis(params.string).then(
+			function(result){
+				res.render('test', { title: 'test', answer:JSON.stringify(result) });
+			},
+			function(error){
+				res.render('test', { title: 'test', answer:error });
+			}
+
+		).done();
 	}
 	
 
